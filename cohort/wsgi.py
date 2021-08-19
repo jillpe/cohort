de39497ac020a3638df5fe2187697f384cb19f47
jillpe <jillpeterson97@gmail.com>
2021-08-19 15:28:52 -0700

    first model and index and create

--- cohort/wsgi.py
+++ cohort/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for finchcollector project.
+WSGI config for cohort project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finchcollector.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cohort.settings')
 
 application = get_wsgi_application()
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,3 @@
 from django.forms import ModelForm
-from .models import Feeding
 
-class FeedingForm(ModelForm):
-    class Meta: 
-        model = Feeding
-        fields = ['date', 'meal']
+
