3d52d9d696fc8f85241ea257c9466c361a10744b
Kevin Ayala <kevinayala6969@gmail.com>
2021-08-20 10:46:33 -0700

    kevins edit

--- main_app/models.py
+++ main_app/models.py
@@ -4,11 +4,12 @@ from datetime import date
 from django.contrib.auth.models import User
 
 TAGS = (
-    ('Job is Cool', 'Job is Cool'),
-    ('Job is Aight', 'Job is Aight'),
-    ('Technical Interview...thats tuff', 'Technical Interview...thats tuff'),
-    ('&#128175;', '&#128175;'),
+    ("Job is Cool", "Job is Cool"),
+    ("Job is Aight", "Job is Aight"),
+    ("Technical Interview...thats tuff", "Technical Interview...thats tuff"),
+    ("&#128175;", "&#128175;"),
 )
+
 
 class JobTitle(models.Model):
     name = models.CharField(max_length=300)
@@ -24,21 +25,11 @@ class JobTitle(models.Model):
     def __str__(self):
         return self.name
 
+
 class Comment(models.Model):
     comment = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     jobtitle = models.ForeignKey(JobTitle, on_delete=models.CASCADE, null=True)
-    tag = models.CharField(
-        max_length=200,
-        choices=TAGS,
-        default=TAGS[0][0]
-    )
+    tag = models.CharField(max_length=200, choices=TAGS, default=TAGS[0][0])
 
-    
-
-
-
-
-
-
-
+    # this is kevins edit
