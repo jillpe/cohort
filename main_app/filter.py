7f15d56e5486e47d4a77b285249d4d9e4f3d45bb
Adrian Ferenc <68762863+adrianferenc@users.noreply.github.com>
2021-08-26 10:31:51 -0700

    fixes spacing and changes textfield to charfield

--- main_app/filter.py
+++ main_app/filter.py
@@ -2,7 +2,6 @@ from django.db import models
 import django_filters
 from .models import JobTitle 
 
-
 class JobTitleFilter(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_expr='icontains')
     company = django_filters.CharFilter(lookup_expr='icontains')
